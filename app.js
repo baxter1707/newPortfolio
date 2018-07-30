5139993411a95235f917f73265442c8626ead944
Michael Baxter <michael_steven.baxter@yahoo.com>
2018-07-30 17:23:05 -0500

    updated content

--- app.js
+++ app.js
@@ -23,6 +23,41 @@ app.get('/home', (req,res) => {
   res.render('home')
 })
 
+app.post('/ContactForm', function (req, res) {
+  console.log("this is firing")
+  let mailOpts, smtpTrans;
+  smtpTrans = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'MichaelPortfolioSite17@gmail.com',
+      pass: 'Portfolio1!'
+    }
+  });
+  mailOpts = {
+    from: req.body.name + ' &lt;' + req.body.email + '&gt;',
+    to: 'michaelbaxter1707@gmail.com',
+    subject: req.body.subject,
+    text:
+    `
+    name: ${req.body.name}
+    email:(${req.body.email})
+    subject: (${req.body.subject})
+    message:(${req.body.address})
+
+    `
+  };
+  smtpTrans.sendMail(mailOpts, function (error, response) {
+    if (error) {
+      res.redirect('/home');
+    }
+    else {
+      res.redirect('/home');
+    }
+  });
+});
+
 
 
 
